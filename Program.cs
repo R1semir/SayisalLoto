462d4b161ab8b428287d6e2aee10595f3499827d
Emir <emirkan.ulker9@gmail.com>
2023-06-11 00:29:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SayisalLoto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            int aa;
+            int ab;
+            int ac;
+            int ad;
+            int ae;
+            int ar;
+            int at;
+
+            aa = rastgele.Next(0, 100);
+            ab = rastgele.Next(0, 100);
+            ac = rastgele.Next(0, 100);
+            ad = rastgele.Next(0, 100);
+            ae = rastgele.Next(0, 100);
+            ar = rastgele.Next(0, 100);
+            at = rastgele.Next(0, 100);
+
+            Console.Write(aa + " " + ab + " " + " " + ac + " " + ad + "  " + ae + " " + ar + " " + at);
+            Console.Read();
+        }
+    }
+}
